cba0d70485152c9e94a031e3227ca2156459a690
@ririkarawal <rawalritika342@gmail.com>
2026-01-31 14:29:46 +0545

    unit testing done

--- test/features/auth/data/models/auth_api_model_test.dart
+++ test/features/auth/data/models/auth_api_model_test.dart
@@ -1,4 +1,5 @@
 import 'package:event_planner/features/auth/data/models/auth_api_model.dart';
+import 'package:event_planner/features/auth/domain/entities/auth_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -22,6 +23,39 @@ void main() {
       expect(model.username, 'ritika');
       expect(model.phoneNumber, '9841000000');
       expect(model.profilePicture, 'pic.png');
+    });
+    test('toJson should split fullName into first and last name', () {
+      final model = AuthApiModel(
+        fullName: 'User test',
+        email: 'user@example.com',
+        password: 'password',
+      );
+
+      final json = model.toJson();
+
+      expect(json['firstName'], 'User');
+      expect(json['lastName'], 'test');
+      expect(json['email'], 'user@example.com');
+      expect(json['username'], 'user');
+      expect(json['confirmPassword'], 'password');
+    });
+    test('toEntity should convert AuthApiModel to AuthEntity', () {
+      final model = AuthApiModel(
+        id: '111',
+        fullName: 'Testing Test',
+        email: 'test@test.com',
+        phoneNumber: '9851011111',
+        profilePicture: 'profile.jpg',
+      );
+
+      final entity = model.toEntity();
+
+      expect(entity, isA<AuthEntity>());
+      expect(entity.authId, '111');
+      expect(entity.fullName, 'Testing Test');
+      expect(entity.email, 'test@test.com');
+      expect(entity.phoneNumber, '9851011111');
+      expect(entity.profilePicture, 'profile.jpg');
     });
   });
 }
