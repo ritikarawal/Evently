287c7dd1f00c8bd2c93cbedb485afb03247b7f0f
@ririkarawal <rawalritika342@gmail.com>
2026-01-31 13:23:35 +0545

    unit testing of hive service

--- test/core/services/hive/hive_service_test.dart
+++ test/core/services/hive/hive_service_test.dart
@@ -1,7 +1,45 @@
+import 'dart:io';
+import 'package:event_planner/core/services/hive/hive_service.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:event_planner/core/constants/hive_table_constant.dart';
+import 'package:event_planner/features/auth/data/models/auth_hive_model.dart';
 
 void main() {
-  testWidgets('hive service ...', (tester) async {
-    // TODO: Implement test
+  late HiveService hiveService;
+  late String testPath;
+
+  setUp(() async {
+    // isolated Hive storage
+    testPath = Directory.systemTemp.createTempSync().path;
+    Hive.init(testPath);
+
+    if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
+      Hive.registerAdapter(AuthHiveModelAdapter());
+    }
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
+    hiveService = HiveService();
+  });
+  tearDown(() async {
+    await Hive.close();
+    Directory(testPath).deleteSync(recursive: true);
+  });
+
+  test('should save user to hive and retrieve it by id', () async {
+    final user = AuthHiveModel(
+      authId: '1',
+      fullName: 'Test User',
+      username: 'testuser',
+      email: 'test@example.com',
+      password: '123456',
+    );
+
+    await hiveService.register(user);
+    final result = hiveService.getUserById('1');
+
+    expect(result, isNotNull);
+    expect(result!.authId, '1');
+    expect(result.email, 'test@example.com');
+    expect(result.fullName, 'Test User');
   });
 }
